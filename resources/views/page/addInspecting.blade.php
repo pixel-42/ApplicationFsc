8ab365664df2b1bf501bc2b3c78474039dcae868
Anastas456 <56071469+Anastas456@users.noreply.github.com>
2020-05-12 14:36:58 +0300

    Add files via upload

---
+++ resources/views/page/addInspecting.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app.index')
+
+@section('content')
+    <div class="card-body">
+        <h1 class="card-title">Добавление инспектора</h1>
+        <br>
+
+        <form class="forms-sample" action="{{route('add-inspecting')}}" method="post">
+            @csrf
+            <div style="display: flex;">
+            <div class="col-sm-12 col-md-10 col-lg-8 col-xl-6">
+                <div class="card">
+                    <div class="form-group row">
+                        <label for="name" class="col-md-4 col-form-label text-md-right">Имя</label>
+
+                        <div class="col-md-6">
+                            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="email" class="col-md-4 col-form-label text-md-right">E-Mail</label>
+
+                        <div class="col-md-6">
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label for="password" class="col-md-4 col-form-label text-md-right">Пароль</label>
+
+                        <div class="col-md-6">
+                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                            @enderror
+                        </div>
+
+                    </div>
