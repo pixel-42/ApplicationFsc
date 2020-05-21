b16f433c5e6719c283db0cf8502ba280896859b7
mserygin <57643698+mserygin@users.noreply.github.com>
2020-05-21 15:34:10 +0300

    Update OrganisationRequest.php

--- app/Http/Requests/OrganisationRequest.php
+++ app/Http/Requests/OrganisationRequest.php
@@ -11,20 +11,5 @@ class OrganisationRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
-    {
-        return true;
-    }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
-    {
-        return [
-            //
-        ];
-    }
 }
